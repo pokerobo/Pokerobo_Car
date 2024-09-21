6881a9c8417382b513b86e2ffbf2e1298ff7474f
pnhung177 <pnhung177@gmail.com>
2024-09-21 21:08:03 +0700

    Create the LineDetector5Channels class for the 5-ways TCRT5000 module

--- src/Line_Detector.cpp
+++ src/Line_Detector.cpp
@@ -10,7 +10,7 @@
 
 PCF8574 ex0(0x20);
 
-int LineDetector::begin() {
+int LineDetector5Channels::begin() {
   pinMode(ex0, PCF8574_P0, INPUT);
   pinMode(ex0, PCF8574_P1, INPUT);
   pinMode(ex0, PCF8574_P2, INPUT);
@@ -19,12 +19,16 @@ int LineDetector::begin() {
   return 0;
 }
 
-uint8_t LineDetector::read() {
+uint8_t LineDetector5Channels::numOfBits() {
+  return 5;
+}
+
+uint8_t LineDetector5Channels::read() {
   uint8_t flag;
-  flag |= digitalRead(ex0, PCF8574_P0);
-  flag |= digitalRead(ex0, PCF8574_P1) << 1;
+  flag |= digitalRead(ex0, PCF8574_P0) << 4;
+  flag |= digitalRead(ex0, PCF8574_P1) << 3;
   flag |= digitalRead(ex0, PCF8574_P2) << 2;
-  flag |= digitalRead(ex0, PCF8574_P3) << 3;
-  flag |= digitalRead(ex0, PCF8574_P4) << 4;
+  flag |= digitalRead(ex0, PCF8574_P3) << 1;
+  flag |= digitalRead(ex0, PCF8574_P4);
   return flag;
 }
--- src/Line_Detector.h
+++ src/Line_Detector.h
@@ -5,7 +5,15 @@
 
 class LineDetector {
   public:
+    virtual int begin();
+    virtual uint8_t numOfBits();
+    virtual uint8_t read();
+};
+
+class LineDetector5Channels: public LineDetector {
+  public:
     int begin();
+    uint8_t numOfBits();
     uint8_t read();
 };
 
