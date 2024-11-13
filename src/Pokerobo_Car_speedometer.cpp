ba2a706ae53bc33356df7856aba945f8cf25d724
pnhung177 <pnhung177@gmail.com>
2024-11-13 19:02:01 +0700

    Add the Pokerobo_Car_speedometer module

---
+++ src/Pokerobo_Car_speedometer.cpp
@@ -0,0 +1,16 @@
+#include "Constant.h"
+
+ConstantOfCar speedometerConstant(
+  A0, // pinOfRF24CE_,
+  10, // pinOfRF24CSN_,
+   9, // pinOfL298nEnA_,
+   8, // pinOfL298nIn1_,
+   7, // pinOfL298nIn2_,
+   4, // pinOfL298nIn3_,
+   5, // pinOfL298nIn4_,
+   6, // pinOfL298nEnB_,
+  A2, // pinOfTrigger_,
+  A3, // pinOfEcho_,
+  A1, // pinOfServo_,
+  false
+);
---
+++ src/Pokerobo_Car_speedometer.h
@@ -0,0 +1,11 @@
+#ifndef __POKEROBO_CAR_SPEEDOMETER_H__
+#define __POKEROBO_CAR_SPEEDOMETER_H__
+
+#include "Constant.h"
+
+extern ConstantOfCar speedometerConstant;
+
+#include "Pokerobo_Car_base.h"
+#include "Motor_Speed_Calculator.h"
+
+#endif
